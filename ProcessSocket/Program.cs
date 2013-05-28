26e5e640db6bbba101b8793561ce84c6ecbc1df4
Rafael Nascimento Bezerra <rafael.bezerra@vtex.com.br>
2013-05-28 15:02:33 -0300

    Servidor e cliente (exemplos)

---
+++ ProcessSocket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadServer = new Thread(new ThreadStart(() =>
+            {
+                Server server = new Server();
+            }));
+            
+            Thread threadClient = new Thread(new ThreadStart(() =>
+            {
+                Client client = new Client();
+            }));
+
+            threadServer.Start();
+            threadClient.Start();
+            
+        }
+    }
+}
---
+++ ProcessSocket/Server.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessSocket
+{
+    public class Server
+    {
+
+
+        // Incoming data from the client.
+        public string data = null;
+
+        public Server()
+        {
+
+        }
+
+        //public Server()
+        //{
+        //    Process p = new Process();
+        //    ProcessStartInfo process = new ProcessStartInfo("cmd.exe");
+        //    process.CreateNoWindow = true;
+        //    process.UseShellExecute = false;
+        //    process.RedirectStandardOutput = true;
+        //    process.RedirectStandardInput = true;
+        //    process.RedirectStandardError = true;
+
+        //    p.StartInfo = process;
+
+        //    p.Start();
+
+        //    StreamReader reader = p.StandardOutput;
+        //    StreamWriter writer = p.StandardInput;
+        //    StreamReader readerErro = p.StandardError;
+
+        //    Thread thrRead = new Thread(new ThreadStart(() =>
+        //    {
+        //        char[] buffer = new char[1024];
+        //        int totalRead = 0;
+        //        while (true)
+        //        {
+        //            totalRead = reader.Read(buffer, 0, buffer.Length);
+
+        //            string msg = new string(buffer, 0, totalRead);
+
+        //            Console.Write(msg);
+
+        //            Thread.Sleep(100);
+        //        }
+        //    }));
+
+        //    thrRead.Start();
+
+
+
+        //    while (true)
+        //    {
+        //        writer.Write(Console.ReadLine() + "\n");
+        //        writer.Flush();
+        //        Thread.Sleep(100);
+        //    }
+        //}
+
+        public void StartListening()
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+    }
+}
